740da4994d683d0a3bf3d49f65378bff9d6ddb75
info <58048398+kaushikappani@users.noreply.github.com>
2021-05-05 13:35:08 +0530

    add directly from webpage

--- event.js
+++ event.js
@@ -7,20 +7,30 @@ var menuItem = {
 chrome.contextMenus.create(menuItem);
 
 chrome.contextMenus.onClicked.addListener(function (clickData) {
-    let data = ({
+    $.ajax({
         url: clickData.linkUrl,
-        group: localStorage.getItem('selected'),
-        title: clickData.linkUrl,
-        favIconUrl: `https://s2.googleusercontent.com/s2/favicons?domain_url=${clickData.linkUrl}` || 'icon.png'
+        complete: function (htmlData) {
+            var response = htmlData.responseText;
+            var title = response.match(/<title[^>]*>([^<]+)<\/title>/)[1];
+            let data = ({
+                url: clickData.linkUrl,
+                group: localStorage.getItem('selected'),
+                title,
+                favIconUrl: `https://s2.googleusercontent.com/s2/favicons?domain_url=${clickData.linkUrl}` || 'icon.png'
 
-    })
-    localStorage.setItem(`saved-${clickData.linkUrl}`, JSON.stringify(data))
-    var notifOptions = {
-        type: "basic",
-        iconUrl: "icon-128.png",
-        title: `Link Saved! ${new Date()}`,
-        message: `The ${clickData.linkUrl} has been save to ${localStorage.getItem('selected')}`,
-        priority:2
-    };
-    chrome.notifications.create('AddedNotify', notifOptions);
+            })
+            localStorage.setItem(`saved-${clickData.linkUrl}`, JSON.stringify(data))
+            var notifOptions = {
+                type: "basic",
+                iconUrl: "icon-128.png",
+                title: `Link Saved! ${new Date()}`,
+                message: `The ${title} has been save to ${localStorage.getItem('selected')}`,
+                priority: 2
+            };
+            chrome.notifications.create('AddedNotify', notifOptions);
+            response = '';
+        }
+        
+    });
+
 });
